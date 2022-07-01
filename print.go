4dac74fd5dbf2ef8efca1598eaf29d786db4ff75
pengfengyong <pengfengyong@gocyber.world>
2022-07-01 15:34:35 +0800

    main

---
+++ print.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+	fmt.Println("7/3=", 7/3)
+	fmt.Println("3+2.1=", 3+2.1)
+	fmt.Println(true && false)
+	fmt.Print(true || false)
+	fmt.Print(!false)
+	fmt.Printf("\n%s", "hello,world")
+}
